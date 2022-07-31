a104db850ec7ecbeb1920e824aa9725362f96c6c
KristinaKac <kachalkovakristina@mailru>
2022-07-31 19:00:20 +0300

    new commit

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -4,60 +4,28 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.opencsv.CSVReader;
-import com.opencsv.CSVWriter;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import org.w3c.dom.*;
-import org.xml.sax.SAXException;
 
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
-
 public class Main {
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-        // Работа с CSV файлом
+    static List<Employee> list = new ArrayList<>();
+
+    public static void main(String[] args){
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
-        List<Employee> list = parseCSV(columnMapping, fileName);
+        list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
         writeString(json);
     }
-    private static List<Employee> parseXML(String fileName2) throws ParserConfigurationException, IOException, SAXException {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.parse(fileName2);
-
-        Node root = document.getDocumentElement();
-        System.out.println("Корневой элемент: " + root.getNodeName());
-
-        NodeList nodeList = root.getChildNodes();
-        for (int i = 0; i < nodeList.getLength(); i++) {
-            Node node_ = nodeList.item(i);
-            if (Node.ELEMENT_NODE == node_.getNodeType()) {
-                System.out.println("Текущий узел: " + node_.getNodeName());
-                Element element = (Element) node_;
-                NamedNodeMap map = element.getAttributes();
-                for (int a = 0; a < map.getLength(); a++) {
-                    String attrName = map.item(a).getNodeName();
-                    String attrValue = map.item(a).getNodeValue();
-                    System.out.println("Атрибут: " + attrName + "; значение: " + attrValue);
-                }
-            }
-        }
-      return null;
-    }
     private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
-
         try (CSVReader csvReader = new CSVReader(new FileReader(fileName))) {
             ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
             strategy.setType(Employee.class);
@@ -65,13 +33,12 @@ public class Main {
             CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader)
                     .withMappingStrategy(strategy)
                     .build();
-            List<Employee> list = csv.parse();
+            list = csv.parse();
             list.forEach(System.out::println);
-            return list;
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return list;
     }
     private static String listToJson(List<Employee> list){
         GsonBuilder builder = new GsonBuilder();
